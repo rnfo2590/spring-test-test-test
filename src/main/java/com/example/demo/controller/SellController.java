bc41abf996962d9ab795235292341779da2baa00
student <student@WIN-R9M3320BL7P>
2024-06-18 13:02:38 +0900

    くるみ01

--- src/main/java/com/example/demo/controller/SellController.java
+++ src/main/java/com/example/demo/controller/SellController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -7,18 +8,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.entity.Category;
-import com.example.demo.entity.Customer;
-import com.example.demo.entity.Item;
 import com.example.demo.model.Account;
 import com.example.demo.repository.ItemRepository;
 
-import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -28,9 +26,6 @@ public class SellController {
 
 	@Autowired
 	Account account;
-
-	@Autowired
-	Item item;
 
 	@Autowired
 	ItemRepository itemRepository;
@@ -48,7 +43,7 @@ public class SellController {
 			@RequestParam(name = "condition", required = false) String condition,
 			@RequestParam(name = "price", required = false) Integer price,
 			@RequestParam(name = "image", required = false) MultipartFile image,
-			@RequestParam(name = "detail", required = false) String detail) {
+			@RequestParam(name = "detail", required = false) String detail) throws IOException {
 
 		// エラーチェック
 		List<String> errorList = new ArrayList<>();
@@ -77,7 +72,7 @@ public class SellController {
 		String file = Base64.getEncoder().encodeToString(bytes);
 		System.out.println(fileName);
 
-		m.addAttribute("image", image);
+		m.addAttribute("image", file);
 		m.addAttribute("name", name);
 		m.addAttribute("category", category);
 		m.addAttribute("condition", condition);
@@ -89,15 +84,15 @@ public class SellController {
 	@PostMapping("/sell/regist")
 	public String store(Model m,
 			@RequestParam(name = "name", required = false) String name,
-			@RequestParam(name = "category", required = false) Category category,
+			@RequestParam(name = "category", required = false) String category,
 			@RequestParam(name = "condition", required = false) String condition,
 			@RequestParam(name = "price", required = false) Integer price,
 			@RequestParam(name = "image", required = false) byte[] image,
 			@RequestParam(name = "detail", required = false) String detail) {
 
-		Customer customer = account.getId();
-		Item item = new Item(category, customer, name, price, image, condition, detail);
-		itemRepository.save(item);
+		//		Integer customer = account.getId();
+		//		Item item = new Item(category, customer, name, price, image, condition, detail);
+		//		itemRepository.save(item);
 		return "/sellFinish";
 	}
 
