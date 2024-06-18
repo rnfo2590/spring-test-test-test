d9aea8cf21c82b35d7af76c0af27ef2b8f34d602
student <student@WIN-R9M3320BL7P>
2024-06-18 17:52:12 +0900

    くるみ05

--- src/main/java/com/example/demo/controller/SellController.java
+++ src/main/java/com/example/demo/controller/SellController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.entity.Category;
+import com.example.demo.entity.Item;
 import com.example.demo.model.Account;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.repository.CustomerRepository;
 import com.example.demo.repository.ItemRepository;
 
 import jakarta.servlet.http.HttpSession;
@@ -28,67 +34,70 @@ public class SellController {
 	@Autowired
 	ItemRepository itemRepository;
 
-	@GetMapping("/sell")
-	public String index() {
+	@Autowired
+	CategoryRepository categoryRepository;
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	private Item item;
+
+	@GetMapping("/sell")
+	public String index(Model model) {
+		// 全カテゴリー一覧を取得
+		List<Category> categoryList = categoryRepository.findAll();
+		model.addAttribute("categories", categoryList);
 		return "/sellForm";
 	}
 
 	@PostMapping("/sell")
 	public String create(Model m,
 			@RequestParam(name = "name", required = false) String name,
-			@RequestParam(name = "category", required = false) String category,
+			@RequestParam(name = "categoryId", required = false) Integer categoryId,
 			@RequestParam(name = "condition", required = false) String condition,
 			@RequestParam(name = "price", required = false) Integer price,
 			@RequestParam(name = "image", required = false) MultipartFile image,
 			@RequestParam(name = "detail", required = false) String detail) throws IOException {
 
-		//		// エラーチェック
-		//		List<String> errorList = new ArrayList<>();
-		//		if ((name == null)
-		//				|| (category == null)
-		//				|| (condition == null)
-		//				|| (image == "")
-		//				|| (price == null)
-		//				|| (detail == null)) {
-		//			errorList.add("必須項目が未入力です");
-		//		}
-		//
-		//		if (errorList.size() > 0) {
-		//			m.addAttribute("errorList", errorList);
-		//			m.addAttribute("name", name);
-		//			m.addAttribute("category", category);
-		//			m.addAttribute("condition", condition);
-		//			m.addAttribute("price", price);
-		//			m.addAttribute("image", image);
-		//			m.addAttribute("detail", detail);
-		//			return "/sellForm";
-		//		}
+		// エラーチェック
+		List<String> errorList = new ArrayList<>();
+		if ((name == null)
+				|| (categoryId == null)
+				|| (condition == null)
+				|| (image == null)
+				|| (price == null)
+				|| (detail == null)) {
+			errorList.add("必須項目が未入力です");
+			m.addAttribute("errorList", errorList);
+			m.addAttribute("name", name);
+			m.addAttribute("categoryId", categoryId);
+			m.addAttribute("condition", condition);
+			m.addAttribute("price", price);
+			m.addAttribute("image", image);
+			m.addAttribute("detail", detail);
+			return "/sellForm";
+		}
 
 		String fileName = StringUtils.cleanPath(image.getOriginalFilename());
-		byte[] bytes = image.getBytes();
-		String file = Base64.getEncoder().encodeToString(bytes);
+		byte[] images = image.getBytes();
+		item = new Item(categoryRepository.findById(categoryId).get(),
+				//new Customer(),
+				customerRepository.findById(account.getId()).get(),
+				name, price, images, condition, detail, 1);
+		//item.setImage(images);
+		String file = Base64.getEncoder().encodeToString(images);
 		System.out.println(fileName);
 
-		m.addAttribute("image", file);
-		m.addAttribute("name", name);
-		m.addAttribute("category", category);
-		m.addAttribute("condition", condition);
-		m.addAttribute("price", price);
-		m.addAttribute("detail", detail);
+		m.addAttribute("item", item);
+
 		return "/sellConfirm";
 	}
 
 	@PostMapping("/sell/regist")
-	public String store(Model m,
-			@RequestParam(name = "name", required = false) String name,
-			@RequestParam(name = "category", required = false) String category,
-			@RequestParam(name = "condition", required = false) String condition,
-			@RequestParam(name = "price", required = false) Integer price,
-			@RequestParam(name = "image", required = false) byte[] image,
-			@RequestParam(name = "detail", required = false) String detail) {
+	public String store() {
 
-		//		itemRepository.save(name, category, condition, price, image, detail);
+		itemRepository.save(item);
+
 		return "/sellFinish";
 	}
 
--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -26,44 +26,61 @@ public class Item {
 	private Integer id; // 商品ID
 
 	@OneToOne
-	@JoinColumn(name="category_id")
+	@JoinColumn(name = "category_id")
 	private Category category; // カテゴリーID
-	
+
 	@OneToOne
-	@JoinColumn(name="customer_id")
+	@JoinColumn(name = "customer_id")
 	private Customer customer; // カスタマーID
-	
+
 	private String name; // 商品名
 
 	private Integer price; // 価格
-	
+
 	@Lob
 	//@Type(type = "org.hibernate.type.BinaryType")
-	@JdbcTypeCode(Types.VARBINARY) 
-	@Column(name="image")
+	@JdbcTypeCode(Types.VARBINARY)
+	@Column(name = "image")
 	private byte[] image;
 
 	private String condition;
-	
+
 	private String detail;
-	
+
 	private Integer stock;
-	
+
 	private String creator = "謙汰";
-	
+
 	private LocalDate date_created = LocalDate.now();
-	
+
 	private String updater = "謙汰";
-	
+
 	private LocalDate date_updata = LocalDate.now();
-	
+
 	private Integer ver_num = 0;
-	
-	private Integer delete_flg = 0; 
-//	@Transient // 永続化対象外
-//	private Integer quantity; // 数量
+
+	private Integer delete_flg = 0;
+	//	@Transient // 永続化対象外
+	//	private Integer quantity; // 数量
 
 	public String getImage() {
 		return Base64.getEncoder().encodeToString(image);
 	}
+
+	public Item() {
+
+	}
+
+	public Item(Category category, Customer customer, String name, Integer price,
+			byte[] image, String condition, String detail, Integer stock) {
+		this.category = category;
+		this.customer = customer;
+		this.name = name;
+		this.price = price;
+		this.image = image;
+		this.condition = condition;
+		this.detail = detail;
+		this.stock = stock;
+	}
+
 }
