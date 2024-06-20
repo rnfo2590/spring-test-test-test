1bb5b450e4cf0f0c8b47b99a46a3bc34783690fb
student <student@WIN-R9M3320BL7P>
2024-06-20 10:53:04 +0900

    データ少し変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,6 +15,6 @@ INSERT INTO customers(name, password, tel, email,address,creator,updater,ver_num
 VALUES('田町','okashi','010987654','jet@gmail.com','渋谷区渋谷2-24-12','香穂','香穂',0,0);
 
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
-VALUES(1, 1, 'Javaの基本', 2500,pg_read_binary_file('C:/dragon.png'), '中古', 'Javaの学習用', 1,'謙汰','謙汰',0,0);
+VALUES(1, 1, 'Javaの基本', 2500,pg_read_binary_file('C:/dragon.png'), '傷や汚れあり', 'Javaの学習用', 1,'謙汰','謙汰',0,0);
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
-VALUES(1, 2,  '呪術海鮮', 650,pg_read_binary_file('C:/jyujyu.png'), '新品', '呪術海鮮最新刊', 1,'香穂','香穂',0,0);
+VALUES(1, 2,  '呪術海鮮', 650,pg_read_binary_file('C:/jyujyu.png'), '新品、未使用', '呪術海鮮最新刊', 1,'香穂','香穂',0,0);
