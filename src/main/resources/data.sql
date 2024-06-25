a7f697609929d413a86a788156fdfca72d754034
student <student@WIN-GOGTOCCO4NN>
2024-06-25 13:59:53 +0900

    kenta37

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -60,7 +60,7 @@ VALUES(3, 1,  '呪術廻戦　ならぶんです　コンプ品', 5000,pg_read_b
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
 VALUES(4, 1,  'カプシーヌ 2024 SS', 9999999,pg_read_binary_file('C:/bag.jfif'), '未使用に近い', '頂き物です。家で保管していましたが、使用する目処がないので出品しました。', 1,'香穂','香穂',0,0);
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
-VALUES(3, 1,  '♡様 専用', 10000,pg_read_binary_file('C:/Image.jfif'), '新品、未使用', '♡様以外のご購入はお控えください', 1,'香穂','香穂',0,0);
+VALUES(3, 1,  '香穂様 専用', 10000,pg_read_binary_file('C:/Image.jfif'), '新品、未使用', '香穂様以外のご購入はお控えください', 1,'香穂','香穂',0,0);
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
 VALUES(5, 1,  '超音波トリートメント', 10000,pg_read_binary_file('C:/yaman.jpg'), '未使用に近い', '同期からおすすめしてもらいましたが、自分には合わなかったので出品しました。同期曰く髪質改善するみたいです。', 1,'香穂','香穂',0,0);
 
@@ -72,4 +72,13 @@ INSERT INTO items(category_id, customer_id, name, price, image, condition, detai
 VALUES(3, 2, 'ゼニガメ', 7000,pg_read_binary_file('C:/zeni.png'),'新品、未使用','一番かわいいです', 1,'香穂','香穂',0,0);
 INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
 VALUES(3, 1,  'フシギダネ', 5000,pg_read_binary_file('C:/fusi.png'), '新品、未使用', 'バケモノの子', 1,'謙汰','謙汰',0,0);
+
+INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
+VALUES(3, 1, 'りの', 1025,pg_read_binary_file('C:/momotarou.png'),'新品、未使用','絶対支配者', 1,'香穂','香穂',0,0);
+INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
+VALUES(3, 1, 'かほ', 1016,pg_read_binary_file('C:/kiji.png'),'新品、未使用','おしゃべり', 1,'香穂','香穂',0,0);
+INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
+VALUES(3, 1,  'くるみ', 1014,pg_read_binary_file('C:/inu.png'), '新品、未使用', 'よく食べる<br>よく寝る', 1,'謙汰','謙汰',0,0);
+INSERT INTO items(category_id, customer_id, name, price, image, condition, detail, stock, creator,updater,ver_num,delete_flg) 
+VALUES(3, 1,  'けんた', 1015,pg_read_binary_file('C:/saru.png'), '新品、未使用', 'お散歩好き', 1,'謙汰','謙汰',0,0);
 
